e3b7902b23f93022f02cfe4266fd23e6ff3fbc78
Nafisa <nafisa.dcc@gmail.com>
2022-12-19 18:22:04 +0600

    first commit

---
+++ static/js/cart.js
@@ -0,0 +1,7 @@
+var updateBtns = documents.getElementsByClassName('update-cart')
+
+
+for(var i = 0; i< updateBtns.length; i++)
+    updateBtns[i].addEventListener('click', function(){
+        
+    })
